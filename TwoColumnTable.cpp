08775298220502e86977d0cc5772ca85deecf807
Sergey <majo78@mail.ru>
2021-10-31 11:41:00 +0300

    Changed file TwoColumnTable.cpp - creating a table header.

--- TwoColumnTable.cpp
+++ TwoColumnTable.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    //This program creates a two-column table.
+    //Table borders are aligned in the console as the order of numbers increases.
+    std::string firstColumnTitle  = "C";
+    std::string secondColumnTitle = "F";
+
+    for (int i = 0; i <= 60; i++) {
+        if (i > 20 && i < 40) {
+            std::cout << "-";
+        } else if (i == 20 || i == 40 ){
+            std::cout << "|";
+        } else {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+    for (int i = 0; i <= 60; i++) {
+        if (i == 20 || i == 30 || i == 40) {
+            std::cout << "|";
+        } else if (i == 25) {
+            std::cout << firstColumnTitle;
+        } else if (i == 35) {
+            std::cout << secondColumnTitle;
+        } else {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i <= 60; i++) {
+        if (i > 20 && i < 30 || i > 30 && i < 40) {
+            std::cout << "-";
+        } else if (i == 20 || i == 30 || i == 40){
+            std::cout << "|";
+        } else {
+            std::cout << " ";
+        }
+    }
 }
