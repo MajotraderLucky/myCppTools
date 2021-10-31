9e8d02b6ff86b7a9350caf21efd34c8e44056f1e
Sergey <majo78@mail.ru>
2021-10-31 21:05:02 +0300

    Now the title adapts to the number of letters in the column titles.

--- TwoColumnTable.cpp
+++ TwoColumnTable.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <string>
 
 int main() {
     //This program creates a two-column table.
     //Table borders are aligned in the console as the order of numbers increases.
-    std::string firstColumnTitle  = "C";
-    std::string secondColumnTitle = "F";
+    std::string firstColumnTitle  = "CIFA";
+    std::string secondColumnTitle = "FAAAAhhhh";
 
-    for (int i = 0; i <= 60; i++) {
-        if (i > 20 && i < 30 || i > 30 && i < 40) {
+    int firstColumnTitleSize  = firstColumnTitle.size()  - 1;
+    int secondColumnTitleSize = secondColumnTitle.size() - 1;
+
+    std::cout << firstColumnTitleSize << std::endl;
+
+    for (int i = 0; i <= 60 + firstColumnTitleSize; i++) {
+        if (i > 20 && i < (30 + firstColumnTitleSize) || i > (30 + firstColumnTitleSize) && i < 40 + firstColumnTitleSize + secondColumnTitleSize) {
             std::cout << "-";
-        } else if (i == 20 || i == 30 || i == 40){
+        } else if (i == 20 || i == 30 + firstColumnTitleSize || i == 40 + firstColumnTitleSize + secondColumnTitleSize){
             std::cout << "|";
         } else {
             std::cout << " ";
@@ -28,14 +34,14 @@ int main() {
         }
     }
     std::cout << std::endl;
-
-    for (int i = 0; i <= 60; i++) {
-        if (i > 20 && i < 30 || i > 30 && i < 40) {
+    for (int i = 0; i <= 60 + firstColumnTitleSize; i++) {
+        if (i > 20 && i < (30 + firstColumnTitleSize) || i > (30 + firstColumnTitleSize) && i < 40 + firstColumnTitleSize + secondColumnTitleSize) {
             std::cout << "-";
-        } else if (i == 20 || i == 30 || i == 40){
+        } else if (i == 20 || i == 30 + firstColumnTitleSize || i == 40 + firstColumnTitleSize + secondColumnTitleSize){
             std::cout << "|";
         } else {
             std::cout << " ";
         }
     }
+    std::cout << std::endl;
 }
